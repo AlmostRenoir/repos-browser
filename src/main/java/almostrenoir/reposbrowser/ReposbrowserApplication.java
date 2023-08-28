7fe5eac834eb3e79003fe28b72928a2dce55ca12
Rafał Augustyn <r.augustyn96@gmail.com>
2023-08-28 07:20:39 +0200

    init commit

---
+++ src/main/java/almostrenoir/reposbrowser/ReposbrowserApplication.java
@@ -0,0 +1,13 @@
+package almostrenoir.reposbrowser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReposbrowserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReposbrowserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/almostrenoir/reposbrowser/ReposbrowserApplicationTests.java
@@ -0,0 +1,13 @@
+package almostrenoir.reposbrowser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReposbrowserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
